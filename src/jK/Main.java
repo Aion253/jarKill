1647984c335ce8e1d6d6313bbbd747ce1b8280fa
Frostbite253 <wr.paratekk@gmail.com>
2016-07-25 23:22:50 -0700

    Added Java file

---
+++ src/jK/Main.java
@@ -0,0 +1,51 @@
+package jK;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) {
+		 String processName = "chrome.exe";
+
+		 //System.out.print(isProcessRunning(processName));
+
+		 try {
+			if (isProcessRunning(processName)) {
+
+			  killProcess(processName);
+			 }
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	private static final String TASKLIST = "tasklist";
+	private static final String KILL = "taskkill /F /IM ";
+
+	public static boolean isProcessRunning(String serviceName) throws Exception {
+
+	 Process p = Runtime.getRuntime().exec(TASKLIST);
+	 BufferedReader reader = new BufferedReader(new InputStreamReader(
+	   p.getInputStream()));
+	 String line;
+	 while ((line = reader.readLine()) != null) {
+
+	  System.out.println(line);
+	  if (line.contains(serviceName)) {
+	   return true;
+	  }
+	 }
+
+	 return false;
+
+	}
+
+	public static void killProcess(String serviceName) throws Exception {
+
+	  Runtime.getRuntime().exec(KILL + serviceName);
+
+	 }
+
+}
